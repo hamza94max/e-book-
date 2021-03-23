aa196446d26dfc92304411c4cc0f3f89e3f4bb5d
hamza94max <hamzagamal854@gmail.com>
2021-03-23 13:31:13 +0200

    Switch

--- app/src/main/java/com/example/hamza/ebook/Read.java
+++ app/src/main/java/com/example/hamza/ebook/Read.java
@@ -14,78 +14,89 @@ import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 public class Read extends AppCompatActivity implements OnPageChangeListener {
     int pageNumber =0;
     private String pdfFileName;
+    int savepage;
+    PDFView pdfView;
+    private String PDF_FILE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
 
-        PDFView pdfView = (PDFView) findViewById(R.id.pdfViewe);
+         pdfView = findViewById(R.id.pdfViewe);
 
 
+        SharedPreferences mySharedPreferences = getPreferences(Context.MODE_PRIVATE);
 
+        savepage = mySharedPreferences.getInt("retrievedPage",0);
+        pageNumber = savepage;
 
         int BookId = getIntent().getIntExtra("EXTRA_SESSION_ID",0);
 
         switch (BookId){
 
             case 1:
-                SharedPreferences prefs = getSharedPreferences("pager", MODE_PRIVATE);
-                int malat = prefs.getInt("page", 0); //0 is the default value.
-
                 pdfFileName ="مآلات الخطاب المدني";
-                pdfView.fromAsset(("malat.pdf"))
-                        .onPageChange(this)
-                        .defaultPage(malat)
-                        .enableAnnotationRendering(true)
-                        .spacing(2)
-                        .load();
-                Toast.makeText(this, "توقفت عند "+ " "+(malat+1), Toast.LENGTH_SHORT).show();
+                PDF_FILE="malat.pdf";
+                displayFromAsset(PDF_FILE);
             break;
+            
             case 2:
                 pdfFileName ="الطريق إلى القرآن";
-                prefs = getSharedPreferences("pager", MODE_PRIVATE);
-                int pageid = prefs.getInt("way", 0); //0 is the default value.
-                pdfView.fromAsset(("way.pdf")).onPageChange(this)
-                        .defaultPage(0)
-                        .enableAnnotationRendering(true)
-                        .spacing(2)
-                        .load();
-                // Toast.makeText(this, "توقفت عند "+ " "+(pageid+1), Toast.LENGTH_SHORT).show();
-                break;
+                PDF_FILE="way.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
+
             case 3:
                 pdfFileName ="رقائق القرآن";
-                pdfView.fromAsset(("raq.pdf")).onPageChange(this).load();
-
-                break;
+                PDF_FILE="raq.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
 
             case 4:
                 pdfFileName ="مسلكيات";
-                pdfView.fromAsset(("masl.pdf")).onPageChange(this).load();
-                break;
+                PDF_FILE="masl.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
+
             case 5:
                 pdfFileName ="سُلطة الثقافة الغالِبة";
-                pdfView.fromAsset(("sulta.pdf")).onPageChange(this).load();
-                break;
+                PDF_FILE="sulta.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
 
             case 6:
                 pdfFileName ="التأويل الحداثي للتراث";
-                pdfView.fromAsset(("tawel.pdf")).onPageChange(this).load();
-                break;
+                PDF_FILE="tawel.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
 
             case 7:
                 pdfFileName ="الماجريات";
-                pdfView.fromAsset(("mag.pdf")).onPageChange(this).load();
-                break;
+                PDF_FILE="mag.pdf";
+                displayFromAsset(PDF_FILE);
+            break;
         }
+    }
+
+
+
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+
+        pdfView.fromAsset(PDF_FILE)
+                .defaultPage(pageNumber)
+                .enableSwipe(true)
+                .swipeHorizontal(false)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .load();
     }
 
     @Override
     public void onPageChanged(int page, int pageCount) {
         pageNumber = page;
         setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
-        SharedPreferences.Editor editor = getSharedPreferences("pager", MODE_PRIVATE).edit();
-        editor.putInt("page", pageNumber);
-        editor.apply();
-
     }
 }
