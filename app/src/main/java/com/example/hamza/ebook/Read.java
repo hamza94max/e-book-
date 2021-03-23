4fafbd46abc0a84d0ea83f8d4aefa70dc007552d
hamza94max <hamzagamal854@gmail.com>
2021-03-23 12:04:54 +0200

    Switch

--- app/src/main/java/com/example/hamza/ebook/Read.java
+++ app/src/main/java/com/example/hamza/ebook/Read.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
@@ -24,56 +25,58 @@ public class Read extends AppCompatActivity implements OnPageChangeListener {
 
 
         int BookId = getIntent().getIntExtra("EXTRA_SESSION_ID",0);
-        if (BookId==1){
 
-            SharedPreferences prefs = getSharedPreferences("pager", MODE_PRIVATE);
-            int malat = prefs.getInt("page", 0); //0 is the default value.
+        switch (BookId){
 
-            pdfFileName ="مآلات الخطاب المدني";
-            pdfView.fromAsset(("malat.pdf"))
-                    .onPageChange(this)
-                    .defaultPage(malat)
-                    .enableAnnotationRendering(true)
-                    .spacing(2)
-                    .load();
-            Toast.makeText(this, "توقفت عند "+ " "+(malat+1), Toast.LENGTH_SHORT).show();
+            case 1:
+                SharedPreferences prefs = getSharedPreferences("pager", MODE_PRIVATE);
+                int malat = prefs.getInt("page", 0); //0 is the default value.
 
+                pdfFileName ="مآلات الخطاب المدني";
+                pdfView.fromAsset(("malat.pdf"))
+                        .onPageChange(this)
+                        .defaultPage(malat)
+                        .enableAnnotationRendering(true)
+                        .spacing(2)
+                        .load();
+                Toast.makeText(this, "توقفت عند "+ " "+(malat+1), Toast.LENGTH_SHORT).show();
+            break;
+            case 2:
+                pdfFileName ="الطريق إلى القرآن";
+                prefs = getSharedPreferences("pager", MODE_PRIVATE);
+                int pageid = prefs.getInt("way", 0); //0 is the default value.
+                pdfView.fromAsset(("way.pdf")).onPageChange(this)
+                        .defaultPage(0)
+                        .enableAnnotationRendering(true)
+                        .spacing(2)
+                        .load();
+                // Toast.makeText(this, "توقفت عند "+ " "+(pageid+1), Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                pdfFileName ="رقائق القرآن";
+                pdfView.fromAsset(("raq.pdf")).onPageChange(this).load();
 
+                break;
 
-        }else if (BookId==2){
-            pdfFileName ="الطريق إلى القرآن";
-            SharedPreferences prefs = getSharedPreferences("pager", MODE_PRIVATE);
-            int pageid = prefs.getInt("way", 0); //0 is the default value.
-            pdfView.fromAsset(("way.pdf")).onPageChange(this)
-                    .defaultPage(0)
-                    .enableAnnotationRendering(true)
-                    .spacing(2)
-                    .load();
-           // Toast.makeText(this, "توقفت عند "+ " "+(pageid+1), Toast.LENGTH_SHORT).show();
+            case 4:
+                pdfFileName ="مسلكيات";
+                pdfView.fromAsset(("masl.pdf")).onPageChange(this).load();
+                break;
+            case 5:
+                pdfFileName ="سُلطة الثقافة الغالِبة";
+                pdfView.fromAsset(("sulta.pdf")).onPageChange(this).load();
+                break;
 
-        }else if (BookId==3){
-            pdfFileName ="رقائق القرآن";
-            pdfView.fromAsset(("raq.pdf")).onPageChange(this).load();
-        }else if (BookId==4){
-            pdfFileName ="مسلكيات";
-            pdfView.fromAsset(("masl.pdf")).onPageChange(this).load();
-        }else if (BookId==5){
-            pdfFileName ="سُلطة الثقافة الغالِبة";
-            pdfView.fromAsset(("sulta.pdf")).onPageChange(this).load();
-        }else if (BookId==6){
-            pdfFileName ="التأويل الحداثي للتراث";
-            pdfView.fromAsset(("tawel.pdf")).onPageChange(this).load();
+            case 6:
+                pdfFileName ="التأويل الحداثي للتراث";
+                pdfView.fromAsset(("tawel.pdf")).onPageChange(this).load();
+                break;
+
+            case 7:
+                pdfFileName ="الماجريات";
+                pdfView.fromAsset(("mag.pdf")).onPageChange(this).load();
+                break;
         }
-        else if (BookId==7){
-            pdfFileName ="الماجريات";
-            pdfView.fromAsset(("mag.pdf")).onPageChange(this).load();
-        }
-
-
-
-
-
-
     }
 
     @Override
