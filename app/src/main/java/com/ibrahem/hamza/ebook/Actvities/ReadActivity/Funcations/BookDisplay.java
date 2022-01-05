a120aae37a6c3a06c3c5ca97c33324e3438af516
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-01-05 15:17:19 +0200

    On change page

--- app/src/main/java/com/ibrahem/hamza/ebook/Actvities/ReadActivity/Funcations/BookDisplay.java
+++ app/src/main/java/com/ibrahem/hamza/ebook/Actvities/ReadActivity/Funcations/BookDisplay.java
@@ -1,15 +1,19 @@
 package com.ibrahem.hamza.ebook.Actvities.ReadActivity.Funcations;
 
+import android.content.Context;
+
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.ibrahem.hamza.ebook.Actvities.ReadActivity.Model.BookModel;
 
 import ibrahem.hamza.ebook.databinding.ActivityReadBinding;
 
 public class BookDisplay {
 
-    public static void displayFromAsset(BookModel book, ActivityReadBinding binding) {
+    public static void displayFromAsset(BookModel book, ActivityReadBinding binding, Context context) {
         binding.pdfViewer.fromAsset(book.getAssetFileName())
                 .defaultPage(book.getLastopenedpage())
                 .enableSwipe(true)
+                .onPageChange((OnPageChangeListener) context)
                 .swipeHorizontal(false)
                 .enableAnnotationRendering(true)
                 .load();
